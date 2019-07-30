6817fa271aab1d36494598adc0dff14b6ff6ffe9
Dil Sombayeva <dilyasom@gmail.com>
2019-07-30 06:15:02 -0700

    add validateUser middleware

--- users/userRouter.js
+++ users/userRouter.js
@@ -100,7 +100,21 @@ function validateUserId(req, res, next) {
   });
 }
 
-function validateUser(req, res, next) {}
+function validateUser(req, res, next) {
+ console.log(Object.keys(req.body));
+ if (Object.keys(req.body).length === 0) {
+  res.status(400).json({ message: 'missing user data.' });
+ } else if (!req.body.name) {
+  res.status(400).json({ message: 'missing required name field' });
+ } else {
+  next();
+ }
+ //  if (req.body && Object.keys(req.body).length > 0) {
+ //   next();
+ //  } else {
+ //   res.status(400).json({ message: 'Please include a body in your request.' });
+ //  }
+}
 
 function validatePost(req, res, next) {}
 
