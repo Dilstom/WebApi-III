6443b3b532279a7b5ea6366442c7d2b2be5ac4c2
Dil Sombayeva <dilyasom@gmail.com>
2019-07-30 06:15:31 -0700

    add validatePost middleware

--- users/userRouter.js
+++ users/userRouter.js
@@ -116,6 +116,14 @@ function validateUser(req, res, next) {
  //  }
 }
 
-function validatePost(req, res, next) {}
+function validatePost(req, res, next) {
+ if (Object.keys(req.body).length <= 0) {
+  res.status(400).json({ message: 'missing post data' });
+ } else if (!req.body.text) {
+  res.status(400).json({ message: 'missing required text field' });
+ } else {
+  next();
+ }
+}
 
 module.exports = router;
